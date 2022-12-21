5dac5dfc567db715f9122adf1bca380a364bafa1
pacastro66 <pauloantunes66@hotmail.com>
2022-12-21 16:28:47 -0300

    edição de usuarios funcionando

--- administracao/urls.py
+++ administracao/urls.py
@@ -7,5 +7,6 @@ path('servicos/listar',servico_views.listar_servicos,name='listar_servicos'),
 path('servicos/editar/<int:id>',servico_views.editar_servico,name='editar_servico'),
 path('usuarios/cadastrar',usuario_views.cadastrar_usuario,name='cadastrar_usuario'),
 path('usuarios/listar',usuario_views.listar_usuarios,name='listar_usuarios'),
+path('usuarios/editar/<int:id>',usuario_views.editar_usuario,name='editar_usuario'),
 
 ]
