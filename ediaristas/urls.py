22cd1157715c344499bd79719adcd9d8286c1c06
pacastro66 <pauloantunes66@hotmail.com>
2023-05-17 09:21:46 -0300

    adicionado api ao projeto

--- ediaristas/urls.py
+++ ediaristas/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('administracao/',include('administracao.urls') )
+    path('administracao/',include('administracao.urls') ),
+    path('api/',include('api.urls') ),
 ]
